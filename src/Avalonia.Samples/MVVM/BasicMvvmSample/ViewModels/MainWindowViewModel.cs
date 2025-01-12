bf6e2a568f7222b71cd5139e610b011ff91c617c
Joe Care <Joe_care_99@yahoo.com>
2025-01-12 14:00:24 +0100

    Add a Community-Toolkit ViewModel to the sample

--- src/Avalonia.Samples/MVVM/BasicMvvmSample/ViewModels/MainWindowViewModel.cs
+++ src/Avalonia.Samples/MVVM/BasicMvvmSample/ViewModels/MainWindowViewModel.cs
@@ -5,9 +5,11 @@ namespace BasicMvvmSample.ViewModels
         // Add our SimpleViewModel.
         // Note: We need at least a get-accessor for our Properties.
         public SimpleViewModel SimpleViewModel { get; } = new SimpleViewModel();
-
         
         // Add our ReactiveViewModel
         public ReactiveViewModel ReactiveViewModel { get; } = new ReactiveViewModel();
+
+        // Add our Community-Toolkit-ViewModel
+        public CTViewModel CTViewModel { get; } = new CTViewModel();
     }
 }
